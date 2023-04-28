58a0efb7160fddb449de4fe63b10bf4b72e43993
isaaccorcia <122558853+isaaccorcia@users.noreply.github.com>
2023-04-27 20:03:11 -0400

    Add files via upload

---
+++ LangGuessr/index.js
@@ -0,0 +1,19 @@
+// REMEMBER TO RUN "npm install" in your terminal (in the same directory as this file)
+const franc = require("franc");
+const langs = require("langs");
+const colors = require("colors")
+
+const input = process.argv[2];
+const langCode = franc(input);
+if (langCode === 'und') {
+    console.log("SORRY, COULDN'T FIGURE IT OUT! TRY WITH MORE SAMPLE TEXT!")
+} else {
+    const language = langs.where("3", langCode);
+    if (language) {
+        console.log(colors.green(`Our best guess is: ${language.name}`));
+    }
+    else {
+        console.log(colors.red(`SORRY, COULDN'T FIND A LANGUAGE FOR CODE: ${langCode}`));
+    }
+}
+
